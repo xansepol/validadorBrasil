9c31d3a023371fa61ba603f6fdf34cb5a46d61d2
Alexandre Lopes <xandesepol@gmail.com>
2022-11-29 12:05:06 -0300

    Validação de Placa

---
+++ Keys.cs
@@ -0,0 +1,6 @@
+namespace validadorBrasil{
+    public class Keys
+    {
+        public const string REGEX_PLACA = "^[A-Z]{3}\\d[\\dA-J]\\d{2}$";
+    }
+}
---
+++ StringExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.RegularExpressions;
+namespace validadorBrasil
+{
+    public static class StringExtensions
+    {
+        public static bool IsPlaca(this string placa)
+        {
+            if (string.IsNullOrWhiteSpace(placa))
+                return false;
+
+            return Regex.IsMatch(placa, Keys.REGEX_PLACA, RegexOptions.IgnoreCase);
+        }
+    }
+}
